25fbe7b893057d95fb6b14a71b92b2c55c0b4442
ZY1YOGI <zy1yogi@gmail.com>
2023-07-05 19:02:25 +0300

    add header navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        mukta: "Mukta",
+        museomoderno: "MuseoModerno",
+        "merienda-one": "'Merienda One'",
+        "song-myung": "'Song Myung'",
       },
+    },
+    container: {
+      center: true,
     },
   },
   plugins: [],
