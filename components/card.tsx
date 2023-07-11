aadcad977e4bddadb8e16f8fe879d2d1f8dae172
ZY1YOGI <zy1yogi@gmail.com>
2023-07-11 22:44:02 +0300

    v1.7.0

--- components/card.tsx
+++ components/card.tsx
@@ -13,13 +13,13 @@ interface ICard {
 export default function card({ img, title, region, rate, review }: ICard) {
   return (
     <div className='w-60 rounded-t-3xl'>
-      <Image src={img} alt={title} className='shadow-2xl p-0.5 rounded-lg shadow-gray-900' width={500} height={500}/>
+      <Image src={img} alt={title} className='shadow-2xl p-0.5 rounded-lg shadow-gray-900' width={500} height={500} data-aos="zoom-in" data-aos-duration="1250"/>
       <div className='text-center mt-1.5'>
-        <h2 className='text-3xl tracking-widest'>{title}</h2>
+        <h2 className='text-3xl tracking-widest' data-aos="fade-up" data-aos-duration="750">{title}</h2>
         <h3>{region}</h3>
       </div>
       <div className='flex items-center justify-center mt-3 space-x-3 shadow-2xl shadow-indigo-500'>
-        <span className='flex px-3 bg-gray-300 rounded-lg'><AiFillStar color='FFC700' size={25} />{rate}</span>
+        <span className='flex px-3 bg-gray-300 rounded-lg' data-aos="fade-right" data-aos-duration="500"><AiFillStar color='FFC700' size={25} />{rate}</span>
         <h3>{review} review</h3>
       </div>
     </div>
--- components/services/services.tsx
+++ components/services/services.tsx
@@ -5,12 +5,12 @@ import { cardData } from '@/data';
 export default function Services() {
   return (
     <section className='mt-12 pb-60 bg-[url(/bg-1.svg)] bg-center bg-cover space-y-12'>
-      <h2 className='px-10 py-3 text-2xl font-bold rounded-br-full bg-body-light dark:bg-body-dark max-w-max text-primary'>WHAT WE SERVE</h2>
+      <h2 className='px-10 py-3 text-2xl font-bold rounded-br-full bg-body-light dark:bg-body-dark max-w-max text-primary' data-aos="fade-down-right" data-aos-duration="1000">WHAT WE SERVE</h2>
 
       <div className='container flex flex-col justify-between pl-8 mt-8 text-white md:flex-row'>
-        <h1 className='text-6xl font-song-myung'>Top <br /> Values <br /> For You</h1>
+        <h1 className='text-6xl font-song-myung' data-aos="fade-right" data-aos-duration="1000">Top <br /> Values <br /> For You</h1>
         <div className='max-sm:mt-6'>
-          <h3 className='text-4xl font-song-myung'>Lots of Choices</h3>
+          <h3 className='text-4xl font-song-myung' data-aos="fade-down-right" data-aos-duration="1000">Lots of Choices</h3>
           <p className='pl-3.5 pt-2.5'>
             Discover your dream <br />
             destination from our <br />
@@ -20,7 +20,7 @@ export default function Services() {
           </p>
         </div>
         <div>
-          <h3 className='text-4xl font-song-myung'>Best Your Guide</h3>
+          <h3 className='text-4xl font-song-myung' data-aos="fade-down-right" data-aos-duration="1000">Best Your Guide</h3>
           <p className='pl-3.5 pt-2.5'>
             Experience your <br />
             destination like a local <br />
@@ -32,7 +32,7 @@ export default function Services() {
           </p>
         </div>
         <div>
-          <h3 className='text-4xl font-song-myung'>Easy by Booking</h3>
+          <h3 className='text-4xl font-song-myung' data-aos="fade-down-right" data-aos-duration="1000">Easy by Booking</h3>
           <p className='pl-3.5 pt-2.5'>
             Book your next adventure <br />
             hassle-free with our easy <br />
@@ -44,7 +44,7 @@ export default function Services() {
       </div>
 
       <div className='container text-white'>
-        <h1 className='text-5xl font-song-myung md:text-6xl max-sm:pl-3.5'>Explore Top Destinations</h1>
+        <h1 className='text-5xl font-song-myung md:text-6xl max-sm:pl-3.5' data-aos="fade-right" data-aos-duration="750">Explore Top Destinations</h1>
         <div className='flex flex-col justify-center mt-12 md:flex-row max-sm:items-center md:space-x-8 max-sm:space-y-8'>
           {
             cardData.map((item) => (
