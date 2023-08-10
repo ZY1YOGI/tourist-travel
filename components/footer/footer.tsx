d9fb77c1bc7bbb6db25e4a225fb3b34a30af87f6
ZY1YOGI <zy1yogi@gmail.com>
2023-08-10 20:35:45 +0300

    Fix Styles & fix SEO Nav <> Footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -3,16 +3,15 @@ import Link from 'next/link';
 import { FaFacebook, FaTwitter, FaYoutube, FaLinkedin } from 'react-icons/fa'
 import { AiOutlineSend } from 'react-icons/ai'
 
-export default function footer() {
+export default function Footer() {
   return (
-    <footer className="relative z-10 dark:text-white pt-20 pb-8 lg:pt-[120px] lg:pb-16 max-lg:px-5 ">
+    <footer className="relative z-10 dark:text-white pt-20 pb-8 lg:pt-[120px] lg:pb-16 max-lg:px-5">
       <div className="container mx-auto">
         <div className="flex flex-wrap -mx-3">
           <div className="w-full px-8 sm:w-2/3 lg:w-3/12">
             <div className="w-full mb-10">
-              <Link href="/" className="flex items-center mb-3 space-x-3">
-                <h1 className="relative text-3xl font-bold dark:text-white">TRAVEL TRIAL
-                  <svg width="56" height="29" viewBox="0 0 56 29" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute w-12 -right-6 -top-2">
+              <Link href="/" className="flex items-center mb-3 space-x-3" title='Marsa Alam Holiday'>
+                <h1 className="relative text-2xl md:text-3xl font-bold dark:text-white whitespace-nowrap">MARSA ALAM HOLIDAY <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute w-12 -right-6 -top-2">
                     <path d="M55.6314 14.8267L53.9335 14.1257C42.962 9.5956 30.7319 16.95 29.5892 28.7648L26.1439 19.705C24.5236 15.4442 27.6062 10.8624 32.1634 10.7577L37.6586 10.6315L22.2533 10.9722L19.6858 3.82297L19.4929 5.7026C19.1857 8.69612 16.6641 10.9722 13.6549 10.9722L0.97935 10.9722C11.9699 -1.56775 30.7393 -3.58629 44.1449 6.33002L55.6314 14.8267Z" fill="#2079FF" />
                   </svg>
                 </h1>
@@ -24,7 +23,7 @@ export default function footer() {
               </p>
               <div className="w-full mb-6">
                 <div className="flex items-center">
-                  <Link title='facebook' href="/" target="_blank" className="flex items-center justify-center w-8 h-8 mr-3 text-white rounded-full bg-primary hover:border-primary sm:mr-4 lg:mr-3 xl:mr-4">
+                  <Link title='facebook' href="https://www.facebook.com/profile.php?id=100064009540678" target="_blank" className="flex items-center justify-center w-8 h-8 mr-3 text-white rounded-full bg-primary hover:border-primary sm:mr-4 lg:mr-3 xl:mr-4">
                     <FaFacebook />
                   </Link>
                   <Link title='twitter' href="/" target="_blank" className="flex items-center justify-center w-8 h-8 mr-3 text-white rounded-full bg-primary hover:border-primary sm:mr-4 lg:mr-3 xl:mr-4">
@@ -47,7 +46,7 @@ export default function footer() {
                       <path d="M0.802807 6.05619C0.869358 7.52032 2.16711 8.11928 2.83263 8.11928H5.16193C5.19521 8.11928 5.19521 8.11928 5.19521 8.11928C6.19348 8.05273 7.19175 7.38722 7.19175 6.05619V5.25757C8.28985 5.25757 10.8188 5.25757 11.9169 5.25757V6.05619C11.9169 7.38722 12.9152 8.05273 13.9135 8.11928H13.9467H16.2428C16.9083 8.11928 18.206 7.52032 18.2726 6.05619C18.2726 5.95636 18.2726 5.59033 18.2726 5.25757C18.2726 4.99136 18.2726 4.75843 18.2726 4.72516C18.2726 4.69188 18.2726 4.6586 18.2726 4.6586C18.1727 3.72688 17.84 2.96154 17.2743 2.36258L17.241 2.3293C16.4091 1.56396 15.4109 1.13138 14.6455 0.865169C12.416 0 9.62088 0 9.48778 0C7.52451 0.0332757 6.26003 0.199654 4.36331 0.865169C3.63125 1.0981 2.63297 1.53068 1.80108 2.29603L1.7678 2.3293C1.20212 2.92827 0.869359 3.69361 0.769531 4.62533C0.769531 4.6586 0.769531 4.69188 0.769531 4.69188C0.769531 4.75843 0.769531 4.95809 0.769531 5.22429C0.802807 5.52377 0.802807 5.92308 0.802807 6.05619ZM2.5997 3.12792C3.26521 2.52896 4.09711 2.16292 4.7959 1.89672C6.52624 1.26448 7.65761 1.13138 9.55433 1.0981C9.68743 1.0981 12.2829 1.13138 14.2795 1.89672C14.9783 2.16292 15.8102 2.49568 16.4757 3.12792C16.8417 3.52723 17.0746 4.05964 17.1412 4.69188C17.1412 4.79171 17.1412 4.95809 17.1412 5.22429C17.1412 5.55705 17.1412 5.92308 17.1412 6.02291C17.1079 6.78825 16.3759 6.95463 16.276 6.95463H13.98C13.6472 6.92135 13.1148 6.78825 13.1148 6.05619V4.69188C13.1148 4.42567 12.9485 4.22602 12.7155 4.12619C12.5159 4.05964 6.69262 4.05964 6.49296 4.12619C6.26003 4.19274 6.09365 4.42567 6.09365 4.69188V6.05619C6.09365 6.78825 5.56124 6.92135 5.22848 6.95463H2.93246C2.83263 6.95463 2.10056 6.78825 2.06729 6.02291C2.06729 5.92308 2.06729 5.55705 2.06729 5.22429C2.06729 4.95809 2.06729 4.82498 2.06729 4.72516C2.00073 4.05964 2.23366 3.52723 2.5997 3.12792Z" />
                     </svg>
                   </span>
-                  <span className="tracking-widest">(+20) 000000000</span>
+                  <span className="tracking-widest">(+20) 10 190 934 39</span>
                 </p>
                 <p className="flex items-center text-sm font-medium ">
                   <span className="mr-3 text-primary">
@@ -57,7 +56,7 @@ export default function footer() {
                       <path d="M0.802807 6.05619C0.869358 7.52032 2.16711 8.11928 2.83263 8.11928H5.16193C5.19521 8.11928 5.19521 8.11928 5.19521 8.11928C6.19348 8.05273 7.19175 7.38722 7.19175 6.05619V5.25757C8.28985 5.25757 10.8188 5.25757 11.9169 5.25757V6.05619C11.9169 7.38722 12.9152 8.05273 13.9135 8.11928H13.9467H16.2428C16.9083 8.11928 18.206 7.52032 18.2726 6.05619C18.2726 5.95636 18.2726 5.59033 18.2726 5.25757C18.2726 4.99136 18.2726 4.75843 18.2726 4.72516C18.2726 4.69188 18.2726 4.6586 18.2726 4.6586C18.1727 3.72688 17.84 2.96154 17.2743 2.36258L17.241 2.3293C16.4091 1.56396 15.4109 1.13138 14.6455 0.865169C12.416 0 9.62088 0 9.48778 0C7.52451 0.0332757 6.26003 0.199654 4.36331 0.865169C3.63125 1.0981 2.63297 1.53068 1.80108 2.29603L1.7678 2.3293C1.20212 2.92827 0.869359 3.69361 0.769531 4.62533C0.769531 4.6586 0.769531 4.69188 0.769531 4.69188C0.769531 4.75843 0.769531 4.95809 0.769531 5.22429C0.802807 5.52377 0.802807 5.92308 0.802807 6.05619ZM2.5997 3.12792C3.26521 2.52896 4.09711 2.16292 4.7959 1.89672C6.52624 1.26448 7.65761 1.13138 9.55433 1.0981C9.68743 1.0981 12.2829 1.13138 14.2795 1.89672C14.9783 2.16292 15.8102 2.49568 16.4757 3.12792C16.8417 3.52723 17.0746 4.05964 17.1412 4.69188C17.1412 4.79171 17.1412 4.95809 17.1412 5.22429C17.1412 5.55705 17.1412 5.92308 17.1412 6.02291C17.1079 6.78825 16.3759 6.95463 16.276 6.95463H13.98C13.6472 6.92135 13.1148 6.78825 13.1148 6.05619V4.69188C13.1148 4.42567 12.9485 4.22602 12.7155 4.12619C12.5159 4.05964 6.69262 4.05964 6.49296 4.12619C6.26003 4.19274 6.09365 4.42567 6.09365 4.69188V6.05619C6.09365 6.78825 5.56124 6.92135 5.22848 6.95463H2.93246C2.83263 6.95463 2.10056 6.78825 2.06729 6.02291C2.06729 5.92308 2.06729 5.55705 2.06729 5.22429C2.06729 4.95809 2.06729 4.82498 2.06729 4.72516C2.00073 4.05964 2.23366 3.52723 2.5997 3.12792Z" />
                     </svg>
                   </span>
-                  <span className="tracking-widest">(+20) 000000000</span>
+                  <span className="tracking-widest">(+20) 10 190 934 39</span>
                 </p>
               </div>
             </div>
@@ -66,7 +65,7 @@ export default function footer() {
             <div className="w-full mb-10">
               <h2 className="mb-6 text-3xl font-bold">About</h2>
               <ul>
-                <li><Link href="/about-us" className="footer-link">About Us</Link></li>
+                <li><Link href="/about-us" className="footer-link" title='Marsa Alam Holiday - About Us'>About Us</Link></li>
                 <li><a href="#" className="footer-link">Features</a></li>
                 <li><a href="#" className="footer-link">News</a></li>
                 <li><a href="#" className="footer-link">Menu</a></li>
@@ -77,7 +76,7 @@ export default function footer() {
             <div className="w-full mb-10">
               <h2 className="mb-6 text-3xl font-bold">Company</h2>
               <ul>
-                <li><Link href="/about-us" className="footer-link">About Us</Link></li>
+                <li><Link href="/about-us" className="footer-link" title='Marsa Alam Holiday - About Us'>About Us</Link></li>
                 <li><a href="#" className="footer-link">Partner with us</a></li>
                 <li><Link href="/faq" className="footer-link">FAQ</Link></li>
                 <li><a href="#" className="footer-link">Blog</a></li>
@@ -91,7 +90,7 @@ export default function footer() {
                 <li><a href="#" className="footer-link">Account</a></li>
                 <li><a href="#" className="footer-link">Support Center</a></li>
                 <li><a href="#" className="footer-link">Feedback</a></li>
-                <li><Link href="/contact-us" className="footer-link">Contact us</Link></li>
+                <li><Link href="/contact-us" className="footer-link" title='Marsa Alam Holiday - Contact Us'>Contact us</Link></li>
                 <li><a href="#" className="footer-link">Accessibility</a></li>
               </ul>
             </div>
--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -30,30 +30,30 @@ export default function Navbar() {
 
   return (
     <nav className="flex items-center justify-between py-3 px-[5%] relative sm:px-[6%] lg:px-[8%] md:py-5 border_gradient border-b-8">
-      <Link href="/" className="relative">
+      <Link href="/" className="relative" title='Marsa Alam Holiday'>
         <h1 className="font-museomoderno sm:text-lg md:text-2xl dark:text-white" data-aos="fade-right">MA-HOLIDAY</h1>
-        <svg width="56" height="29" viewBox="0 0 56 29" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute w-12 -right-7 -top-2" data-aos="fade-left" data-aos-duration="1500">
+        <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute w-12 -right-7 -top-2" data-aos="fade-left" data-aos-duration="1500">
           <path d="M55.6314 14.8267L53.9335 14.1257C42.962 9.5956 30.7319 16.95 29.5892 28.7648L26.1439 19.705C24.5236 15.4442 27.6062 10.8624 32.1634 10.7577L37.6586 10.6315L22.2533 10.9722L19.6858 3.82297L19.4929 5.7026C19.1857 8.69612 16.6641 10.9722 13.6549 10.9722L0.97935 10.9722C11.9699 -1.56775 30.7393 -3.58629 44.1449 6.33002L55.6314 14.8267Z" fill="#2079FF" />
         </svg>
       </Link>
       <ul className="navigation" ref={switchMenu}>
-        <li><Link href="/about-us">About</Link></li>
-        <li><a href="#services">Services</a></li>
+        <li><Link href="/about-us" title='Marsa Alam Holiday - About Us'>About</Link></li>
+        <li><a href="/#services" title='Marsa Alam Holiday - Services'>Services</a></li>
         <li className="relative group" id="nav-link-categories">
-          <button className="flex items-center">Trips <BsBarChartSteps className='ml-1.5' /></button>
+          <button className="flex items-center" title='Trips Marsa Alam'>Trips <BsBarChartSteps className='ml-1.5' /></button>
           <ul role="menu" className="group-hover:block font-song-myung z-50 text-center absolute hidden whitespace-nowrap space-y-5 py-5 top-8 left-[50%] max-md:left-[-18%] border-4 border-x-white border-y-primary rounded-xl px-1 bg-body-light dark:bg-body-dark">
             <li><Link className='block' href="/trips/mountain">Mountain Trips</Link></li>
-            <li><Link className='block' href="/trips">Sea Trips</Link></li>
-            <li><Link className='block' href="/trips">Fishing Trips</Link></li>
-            <li><Link className='block' href="/trips">Shore Trips</Link></li>
+            <li><Link className='block' href="/trips" title='Sea Trips'>Sea Trips</Link></li>
+            <li><Link className='block' href="/trips" title='Fishing Trips'>Fishing Trips</Link></li>
+            <li><Link className='block' href="/trips" title='Shore Trips'>Shore Trips</Link></li>
           </ul>
         </li>
-        <li><Link href="/contact-us">Contact</Link></li>
-        <button className='btn-theme' role="switch" id="switch-theme" aria-checked="true" onClick={() => localStorage.getItem('theme') === 'light' ? (document.documentElement.className = 'dark', document.documentElement.setAttribute('data-theme', 'dark'), localStorage.setItem('theme', 'dark')) : (document.documentElement.className = 'light', document.documentElement.setAttribute('data-theme', 'light'), localStorage.setItem('theme', 'light'))}>
+        <li><Link href="/contact-us" title='Marsa Alam Holiday - Contact Us'>Contact</Link></li>
+        <button className='btn-theme' role="switch" aria-label='switch theme' aria-checked="true" onClick={() => localStorage.getItem('theme') === 'light' ? (document.documentElement.className = 'dark', document.documentElement.setAttribute('data-theme', 'dark'), localStorage.setItem('theme', 'dark')) : (document.documentElement.className = 'light', document.documentElement.setAttribute('data-theme', 'light'), localStorage.setItem('theme', 'light'))}>
           <MdDarkMode size={28} />
         </button>
       </ul>
-      <button className="md:hidden" onClick={() => switchMenu.current?.classList.toggle('!flex')} role="switch">
+      <button className="md:hidden" onClick={() => switchMenu.current?.classList.toggle('!flex')} role="switch" aria-label='switch menu' aria-checked='mixed'>
         <FaBars size={25} className="block dark:text-white" />
       </button>
       <div className="text-sm sm:text-lg md:flex md:space-x-3 md:text-2xl sm:font-semibold">
