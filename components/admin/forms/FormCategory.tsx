225c9568f11e694278856664c85c7a9eacd5885b
ZY1YOGI <zy1yogi@gmail.com>
2023-09-01 21:06:09 +0300

    fix

--- components/admin/forms/FormCategory.tsx
+++ components/admin/forms/FormCategory.tsx
@@ -4,27 +4,35 @@ import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { useState } from 'react';
 import { CgSpinner } from 'react-icons/cg'
+import { TagsInput } from "react-tag-input-component"; 
 
 interface IFormInput {
   name: string;
+  keywords: string;
   description: string;
 }
 
 export default function FormCategory() {
   const { register, handleSubmit, setError, formState: { errors } } = useForm<IFormInput>();
   const [loading, setLoading] = useState<boolean>(false)
+  const [keywords, setKeywords] = useState<string[]>([]);
 
   const onSubmit = async (data: IFormInput) => {
     setLoading(true)
 
-    const response = await axios.post('/api/category', {
-      name: data.name
-    })
 
-    if (response.data.status == 422) {
-      setError('name', { message: "response.data.errors.name" })
-      setError('description', { message: "response.data.errors.description" })
-    }
+
+    console.log('====================================');
+    console.log(data);
+    console.log('====================================');
+    // const response = await axios.post('/api/category', {
+    //   name: data.name
+    // })
+
+    // if (response.data.status == 422) {
+    //   setError('name', { message: "Error" })
+    //   setError('description', { message: "Error" })
+    // }
 
     setLoading(false)
   }
@@ -37,6 +45,11 @@ export default function FormCategory() {
         <label htmlFor="input-name" className="x-label">Enter The Name Category <span className='requeued'>*</span></label>
         <input {...register("name", { required: "The Name Category is Requeued" })} id="input-name" className="x-input" placeholder="Name" />
         <span className="block text-red-500">{errors.name && (errors.name.message)}</span>
+      </div>
+      <div className='space-y-3'>
+        <label htmlFor="input-name" className="x-label">Enter The keywords Category <span className='requeued'>*</span></label>
+        <TagsInput value={keywords} onChange={setKeywords} name="tags" placeHolder="tags" />
+        <span className="block text-red-500">{errors.keywords && (errors.keywords.message)}</span>
       </div>
       <div className='space-y-3'>
         <label htmlFor="input-description" className="x-label">Enter The Description Category <span className='requeued'>*</span></label>
